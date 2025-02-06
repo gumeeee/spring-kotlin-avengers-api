b6c2a8bf03fe5831c94c419f992c52f14f819a7f
gumeeee <gaxco123@gmail.com>
2025-02-06 00:04:49 -0300

    chore: created template avengers api first version

---
+++ src/test/kotlin/com/marvel/avengers/avengers_api/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marvel.avengers.avengers_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
