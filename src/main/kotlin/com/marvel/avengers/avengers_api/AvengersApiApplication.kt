2de556aa9b65b8fca3565167dc78d197d7991ef4
gumeeee <gaxco123@gmail.com>
2025-02-12 22:36:30 -0300

    fix: add annotation in app root EnableJpaRepositories

--- src/main/kotlin/com/marvel/avengers/avengers_api/AvengersApiApplication.kt
+++ src/main/kotlin/com/marvel/avengers/avengers_api/AvengersApiApplication.kt
@@ -2,8 +2,10 @@ package com.marvel.avengers.avengers_api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = ["com.marvel.avengers.avengers_api.resource.avenger"])
 class AvengersApiApplication
 
 fun main(args: Array<String>) {
